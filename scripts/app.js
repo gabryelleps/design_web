aed1efdfd5090b51f29e25ad50f0bcc794af2309
gabryelleps <xsmarilia@gmail.com>
2026-08-19 18:01:33 -0300

    acrescentei txt no js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 alert("Olá, mundo!");
+
+var nasc = 2009;
+let nome = "Marília";
+const viva = true;
+let altura = 1.55;
+
+if (viva){
+    let saudacao = "Olá" + nome + "!";
+    let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
+    alert(saudacao+"\n"+msg);
+}else{
+    alert("R.I.P");
+}
