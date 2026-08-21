d43a6845208802d4638ca848e58314304a5d1c17
gabryelleps <xsmarilia@gmail.com>
2026-08-21 13:24:36 -0300

    corrigi erros de sintaxe no código

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 alert("Olá, mundo!");
 
 var nasc = 2009;
-let nome = "Marília";
+let nome = "'Marília'";
 const viva = true;
 let altura = 1.55;
 
 if (viva){
-    let saudacao = "Olá" + nome + "!";
+    let saudacao = "Olá " + nome + "!";
     let msg = `Altura ${altura}m | Idade ${2026-nasc}`;
     alert(saudacao+"\n"+msg);
 }else{
