5fda2470bfdb905d465f702d4332ccb33e491941
gabryelleps <xsmarilia@gmail.com>
2026-09-04 14:27:24 -0300

    cont

--- js/app4.js
+++ js/app4.js
@@ -1,8 +1,12 @@
 let vezes = Number(prompt("Digite o número de vezes: "));
 for (let i = 1; i<=vezes; i++){
-    alert(`Contei ${i} vezes`);
-    if (i%2==0){
-        continue;
+    if (vezes > 100){
+        alert("Valor inválido, digite um valor de 0 a 100");
+        break //quebra totalmente o bloco de repetição
     }
-    alert(`${i} é par`)
+    alert(`Contei ${i} vezes`);
+    if (i%2==0){//é ímpar, continua
+        continue;//se ímpar, continua = voltar para o início
+    }
+    alert(`${i} é par`);
 }
