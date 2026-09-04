8573b161d21058ec082a8de212e30c9285003e3a
gabryelleps <xsmarilia@gmail.com>
2026-09-04 14:27:04 -0300

    break e continue

--- js/app4.js
+++ js/app4.js
@@ -1,4 +1,8 @@
 let vezes = Number(prompt("Digite o número de vezes: "));
 for (let i = 1; i<=vezes; i++){
     alert(`Contei ${i} vezes`);
+    if (i%2==0){
+        continue;
+    }
+    alert(`${i} é par`)
 }
