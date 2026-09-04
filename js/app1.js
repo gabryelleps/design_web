6f8211324e74794a761b4daa50bca54d634abc16
gabryelleps <xsmarilia@gmail.com>
2026-09-04 13:45:56 -0300

    ;

--- js/app1.js
+++ js/app1.js
@@ -2,6 +2,6 @@ let vezes = prompt("Digite a quantidade de vezes");
 vezes = Number(vezes)
 let i = 1;
 while(i<=vezes){
-    alert(`Contei ${i} vez`)
+    alert(`Contei ${i} vez`);
     i = i + 1;
 }
